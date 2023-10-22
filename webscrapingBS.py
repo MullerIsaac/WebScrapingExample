052b5bcc172f921455b9fcc1bf80709dcdc6f13c
MullerIsaac <50156401+MullerIsaac@users.noreply.github.com>
2023-10-21 22:36:08 -0300

    Modificações no ws com beautifulsoup

--- webscrapingBS.py
+++ webscrapingBS.py
@@ -6,17 +6,18 @@ from bs4 import BeautifulSoup
 f = csv.writer(open('mlproducts.csv', 'w'))
 f.writerow(['Title','Price', 'LastPrice','Link'])
 
-page = requests.get('https://www.mercadolivre.com.br/ofertas?container_id=MLB779362-2&page=1')
+for i in range(1, 20):
+    page = requests.get('https://www.mercadolivre.com.br/ofertas?container_id=MLB779362-2&page={i}')
 
-soup = BeautifulSoup(page.text, 'html.parser')
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-products_list = soup.find_all(class_='promotion-item')
+    products_list = soup.find_all(class_='promotion-item')
 
-for products in products_list:
-    title = products.find(class_='promotion-item__title').getText()
-    preco = products.find_all(class_='andes-money-amount__fraction')
-    atual = preco[0].text
-    antigo = preco[1].text if len(preco)>1 else " "
-    link = products.find('a').get('href')
+    for products in products_list:
+        title = products.find(class_='promotion-item__title').getText()
+        preco = products.find_all(class_='andes-money-amount__fraction')
+        atual = preco[0].text
+        antigo = preco[1].text if len(preco)>1 else " "
+        link = products.find('a').get('href')
 
-    f.writerow([title, atual, antigo, link])
+        f.writerow([title, atual, antigo, link])
